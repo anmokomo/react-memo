b3eb791ca5aefb013c9cd308ae9a0765ae5d7226
Anna Koehler <anna@foundrymakes.com>
2022-02-08 20:52:55 -0600

    useMemo demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, memo, useMemo } from 'react'
+
+function Swatch({ params }) {
+  console.log('swatch rendered ', params.color)
+  return (
+    <>
+      <div style={{ width: 75, height: 75, backgroundColor: params.color }}></div>
+      <p>Swatch rendered {params.color}</p>
+    </>
+  )
+}
+
+const MemoedSwatch = memo(Swatch)
 
 function App() {
+  const [appRenderIndex, setAppRenderIndex] = useState(0)
+  const [color, setColor] = useState('red')
+  console.log('App rendered index', appRenderIndex)
+  
+  //returns an object with that color, gets rerun when color changes
+const params = useMemo(() => ({ color }), [color])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+          Re-render App
+        </button>
+        <p>Render index: {appRenderIndex}</p>
+        <button onClick={() => setColor(color === 'red' ? 'blue' : 'red')}>
+          Change color
+          </button>
+        {/* <Swatch params={color} /> */}
+        <MemoedSwatch params={params} />
+        {/* <MemoedSwatch params={{ color }}/> */}
+      </div>
     </div>
   );
 }
